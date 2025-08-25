3129a200b0724f08de89c994036d98c084a083d6
TheBringerOfTruth <og.tamairaia@gmail.com>
2025-08-26 01:03:00 +0800

    Update pyrosetta_sample.py

--- pyrosetta_sample.py
+++ pyrosetta_sample.py
@@ -4,3 +4,9 @@ init()
 pose = pose_from_file("example.pdb")
 score = pose.energies().total_energy()
 print("Rosetta Score:", score)
+from pyrosetta import init, pose_from_file
+
+init()
+pose = pose_from_file("example.pdb")
+score = pose.energies().total_energy()
+print("Rosetta Score:", score)
